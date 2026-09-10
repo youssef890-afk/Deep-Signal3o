213ac5bd0f529725089a3dab4a36b70babbe1222
youssef890-afk <youssefelamyn890@gmail.com>
2026-09-10 14:09:56 +0100

    Add image compression function

--- src/utils/compressImage.ts
+++ src/utils/compressImage.ts
@@ -1,1 +1,14 @@
+import imageCompression from 'browser-image-compression';
 
+export async function compressImage(file: File): Promise<File> {
+  if (!file.type.startsWith('image/')) {
+    return file;
+  }
+
+  return imageCompression(file, {
+    maxSizeMB: 2,
+    maxWidthOrHeight: 1920,
+    useWebWorker: true,
+    initialQuality: 0.82,
+  });
+}
