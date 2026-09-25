e76b3364fd476a85fc9d3205cad28d2cd57eec9f
youssef890-afk <youssef.elamyn890@gmail.com>
2026-09-25 22:53:51 +0000

    Revert "Update App.tsx"
    This reverts commit d7e102aae0930ff469c042f93f709430d1332222.

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,9 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+<Route
+  path="/forgot-password"
+  element={<ForgotPasswordPage />}
+/>
 
-import LoginPage from "./pages/LoginPage";
-import SignupPage from "./pages/SignupPage";
-import ForgotPasswordPage from "./pages/ForgotPasswordPage";
-import ResetPasswordPage from "./pages/ResetPasswordPage";
-
-export default function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignupPage />} />
-
-        <Route
-          path="/forgot-password"
-          element={<ForgotPasswordPage />}
-        />
-
-        <Route
-          path="/reset-password"
-          element={<ResetPasswordPage />}
-        />
-      </Routes>
-    </BrowserRouter>
-  );
-}
+<Route
+  path="/reset-password"
+  element={<ResetPasswordPage />}
+/>
