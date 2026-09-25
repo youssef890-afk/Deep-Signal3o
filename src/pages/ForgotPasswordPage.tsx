4ab1baf3727c6a94481e5e3af2673e224469e60e
youssef890-afk <youssefelamyn890@gmail.com>
2026-09-25 23:40:22 +0100

    Remove commented file path from ForgotPasswordPage

--- src/pages/ForgotPasswordPage.tsx
+++ src/pages/ForgotPasswordPage.tsx
@@ -1,5 +1,3 @@
-// src/pages/ForgotPasswordPage.tsx
-
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { ArrowLeft, Mail, ShieldCheck } from "lucide-react";
